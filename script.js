9ffa0cbd76b3b17108ccd3081bf8aa6059f30b12
Caio Richard <crkautza@gmail.com>
2026-03-07 20:51:34 -0300

    Ambiente preparado.

---
+++ script.js
@@ -0,0 +1,58 @@
+let produtos = [
+    {
+        nome: 'Arroz',
+        preço: 25.90,
+        quantidade: 15,
+        categoria: 'Alimentos',
+    },
+    {
+        nome: 'Feijão',
+        preço: 12.50,
+        quantidade: 8,
+        categoria: 'Alimentos',
+    },
+    {
+        nome: 'Sabonete',
+        preço: 3.75,
+        quantidade: 42,
+        categoria: 'Higiene',
+    },
+    {
+        nome: 'Shampoo',
+        preço: 18.90,
+        quantidade: 12,
+        categoria: 'Higiene',
+    },
+    {
+        nome: 'Camiseta',
+        preço: 45.90,
+        quantidade: 5,
+        categoria: 'Vestuário',
+    },
+    {
+        nome: 'Calça Jeans',
+        preço: 89.90,
+        quantidade: 3,
+        categoria: 'Vestuário',
+    },
+    {
+        nome: 'Café',
+        preço: 15.90,
+        quantidade: 0,
+        categoria: 'Alimentos',
+    },
+    {
+        nome: 'Pasta de Dente',
+        preço: 8.90,
+        quantidade: 23,
+        categoria: 'Higiene',
+    },
+];
+
+let limite = 41;
+let quantidades = [];
+
+function filtrarMinStock(){
+}
+
+filtrarMinStock();
