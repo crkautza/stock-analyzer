c713560a8eda8878691a2b2a56e625e9d2b810c1
Caio Richard <crkautza@gmail.com>
2026-03-08 19:54:19 -0300

    Grande atualização no script.js e melhorias adicionais na página

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Elements
 let filtrarBtn = document.getElementById('filtrarMin');
 let limiteBtn = document.getElementById('limite');
-let input = document.getElementsByTagName('input')[0];
+let promocaoBtn = document.getElementById('promocao');
+let amountInput = document.getElementById('amountInput');
+let priceInput = document.getElementById('priceInput');
+let categoriaBtn = document.getElementById('categoriaBtn');
 
 //Vars & Arrays
 let produtos = [
@@ -55,19 +58,86 @@ let produtos = [
     },
 ];
 
-let limite = 0;
+let amountlimit = 0;
+let pricelimit = 0;
 let quantidades = [];
+let precos = [];
+let categorias = {};
 
 //Event Listener
 filtrarBtn.addEventListener('click', filtrarMinStock);
 limiteBtn.addEventListener('click', definirLimite);
+promocaoBtn.addEventListener('click', filtrarpreco);
+categoriaBtn.addEventListener('click', ValorTotalPorCategoria);
 
+//Functions
 function filtrarMinStock(){
-    console.log(limite);
+    if(amountlimit > 0){
+        produtos.forEach((value) => {
+            let nome = Object.values(value)[0];
+            let preco = Object.values(value)[1];
+            let quantidade = Object.values(value)[2];
+            let categoria = Object.values(value)[3];
+            if(quantidade < amountlimit){
+                quantidades.push({
+                    nome: nome,
+                    preco: preco,
+                    quantidade: quantidade,
+                    categoria: categoria
+                });
+            }
+        });
+        console.log('Quantidades:', quantidades);
+        quantidades = [];
+    }else{
+        console.log('Produtos:', produtos);
+    }
+}
+
+function filtrarpreco(){
+    if(pricelimit > 0){
+        produtos.forEach((value) => {
+            let nome = Object.values(value)[0];
+            let preco = Object.values(value)[1];
+            let quantidade = Object.values(value)[2];
+            let categoria = Object.values(value)[3];
+            if(preco < pricelimit){
+                precos.push({
+                    nome: nome,
+                    preco: preco,
+                    quantidade: quantidade,
+                    categoria: categoria
+                });
+            }
+        });
+        console.log('Preços:', precos);
+        precos = [];
+    }else{
+        console.log('Produtos:', produtos);
+    }
+}
+
+function ValorTotalPorCategoria(){
+        produtos.forEach((value) => {
+            let preco = Object.values(value)[1];
+            let quantidade = Object.values(value)[2];
+            let categoria = Object.values(value)[3];
+            let valorTotal = preco * quantidade;
+            if (categorias[categoria]) {
+                categorias[categoria] += valorTotal;
+            }else{
+                categorias[categoria] = valorTotal;
+            }
+        });
+        console.log('Categorias:', categorias);
+        categorias = {};
 }
 
 function definirLimite(){
-    if(input.value){
-        limite = input.value;
+    if(amountInput.value){
+        amountlimit = parseInt(amountInput.value);
+    }
+    if(priceInput.value){
+        pricelimit = parseInt(priceInput.value);
     }
 }
